7f29209a6020392a1dd1bddeb68cead8c529d49a
yunha <helena0228c@gmail.com>
2025-10-23 20:02:12 +0900

    merge develop

--- mysql/migrations/2025101023_02_add_review_summaries_review_count_column.sql
+++ mysql/migrations/2025101023_02_add_review_summaries_review_count_column.sql
@@ -1,2 +1,4 @@
-ALTER TABLE `travel_shoot`.`review_summaries`
-ADD COLUMN `review_count` INT NOT NULL DEFAULT 0 AFTER `updated_at`;
+ALTER TABLE review_summaries
+ADD COLUMN review_count INT NOT NULL DEFAULT 0 AFTER updated_at;
+
+COMMIT;
